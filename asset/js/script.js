c7bdb85c0708347c10872b29d202fbd16fe3e8b6
lasi23 <lsilvere39@gmail.com>
2025-09-17 11:25:26 +0200

    exPrixHT

---
+++ asset/js/script.js
@@ -0,0 +1,25 @@
+let bouttonCalcul = document.querySelector("#calculer");
+
+let nomProduit = document.querySelector("#nom_produit");
+let quantite = document.querySelector("#quantite");
+let prixHt = document.querySelector("#prix_ht");
+
+let resultat = document.querySelector("#resultat");
+
+
+
+
+bouttonCalcul.addEventListener("click", () => {
+    
+        if(nomProduit.value || quantite.value, prixHt.value === ""){
+        console.log("ok");
+        resultat.innerText = "champs à remplir";
+        return;
+    }
+    if(isNaN(quantite.value) || isNaN(prixHt.value)){
+        resultat.innerText = "Vérifier les donnés";
+    }else{
+        let calcul = quantite.value * prixHt.value;
+        resultat.innerText = calcul;
+    }
+})
